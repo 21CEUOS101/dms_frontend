3c0157756f532791e51adf92431c133417f92823
21CEUOS101 <21ceuos101@ddu.ac.in>
2023-09-16 16:46:28 +0530

    afsd

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 function Profile(props) {
+
+    const user = props.data;
   return (
       <>
         <div>Profile</div>
-        <p>{JSON.stringify(props.data)}</p>
+        
       </>
   )
 }
