be57bf30daa35cb5a47c397ac5128a6de0fa3bf3
manthan161203 <manthanlp3204@gmail.com>
2023-09-21 18:49:05 +0530

    change time table

--- src/Components/TimeTable.js
+++ src/Components/TimeTable.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import './TimeTable.css';
 
+// Define time slots
+const timeSlots = [
+  '8:30 AM - 9:30 AM',
+  '9:30 AM - 10:30 AM',
+  '10:30 AM - 11:30 AM',
+  '11:30 AM - 12:30 PM',
+  '12:30 PM - 1:30 PM',
+  '1:30 PM - 2:30 PM',
+  '2:30 PM - 3:30 PM',
+  '3:30 PM - 4:30 PM',
+  '4:30 PM - 5:30 PM',
+];
+
+// Function to find the closest matching time slot
+function findClosestTimeSlot(time) {
+  // Add your logic here to find the closest time slot based on the provided time
+  // For example, you can split the time string and compare it with the time slots
+  // You may need to handle AM/PM logic as well
+  // Return the closest time slot string
+  // For now, let's assume a simple implementation
+  return timeSlots.find((slot) => slot === time) || timeSlots[0];
+}
+
 function TimeTable({ data }) {
   // Group data by day and time
   const groupedData = data.reduce((acc, entry) => {
@@ -9,22 +32,17 @@ function TimeTable({ data }) {
     if (!acc[day]) {
       acc[day] = {};
     }
-    acc[day][time] = entry;
+
+    // Find the closest matching time slot
+    const closestTimeSlot = findClosestTimeSlot(time);
+
+    if (!acc[day][closestTimeSlot]) {
+      acc[day][closestTimeSlot] = [];
+    }
+
+    acc[day][closestTimeSlot].push(entry);
     return acc;
   }, {});
-
-  // Define time slots
-  const timeSlots = [
-    '8:30 AM - 9:30 AM',
-    '9:30 AM - 10:30 AM',
-    '10:30 AM - 11:30 AM',
-    '11:30 AM - 12:30 PM',
-    '12:30 PM - 1:30 PM',
-    '1:30 PM - 2:30 PM',
-    '2:30 PM - 3:30 PM',
-    '3:30 PM - 4:30 PM',
-    '4:30 PM - 5:30 PM',
-  ];
 
   // Define days
   const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
@@ -47,11 +65,13 @@ function TimeTable({ data }) {
               {days.map((day) => (
                 <td key={day}>
                   {groupedData[day] && groupedData[day][timeSlot] ? (
-                    <div className="timetable-entry">
-                      <p>{groupedData[day][timeSlot].time_table_block_subject}</p>
-                      <p>{groupedData[day][timeSlot].time_table_block_faculty}</p>
-                      <p>{groupedData[day][timeSlot].time_table_block_room_no}</p>
-                    </div>
+                    groupedData[day][timeSlot].map((entry) => (
+                      <div className="timetable-entry" key={entry.time_table_block_id}>
+                        <p>{entry.time_table_block_subject}</p>
+                        <p>{entry.time_table_block_faculty}</p>
+                        <p>{entry.time_table_block_room_no}</p>
+                      </div>
+                    ))
                   ) : (
                     ''
                   )}
