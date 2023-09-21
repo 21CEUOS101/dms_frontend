06c0898edad607f5cdd53c75999d910b4a0a1510
manthan161203 <manthanlp3204@gmail.com>
2023-09-21 22:14:25 +0530

    Changes in timetable

--- src/Components/TimeTable.js
+++ src/Components/TimeTable.js
@@ -1,6 +1,4 @@
-import React, { useRef } from 'react';
-import html2canvas from 'html2canvas';
-import jsPDF from 'jspdf';
+import React from 'react';
 import './TimeTable.css';
 
 // Define time slots
@@ -18,24 +16,18 @@ const timeSlots = [
 
 // Function to find the closest matching time slot
 function findClosestTimeSlot(time) {
+  // Add your logic here to find the closest time slot based on the provided time
+  // For example, you can split the time string and compare it with the time slots
+  // You may need to handle AM/PM logic as well
+  // Return the closest time slot string
+  // For now, let's assume a simple implementation
   return timeSlots.find((slot) => slot === time) || timeSlots[0];
 }
 
-function TimeTable({ data, timetableId }) {
-  const timetableRef = useRef(null);
-
-  const generatePDF = () => {
-    const input = timetableRef.current;
-
-    html2canvas(input).then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
-      const pdf = new jsPDF('p', 'mm', 'a4');
-      pdf.addImage(imgData, 'PNG', 0, 0, 210, 297); // Adjust width and height as needed
-      pdf.save('timetable.pdf');
-    });
-  };
+function TimeTable({ data , timetableId }) {
 
   const filteredData = data.filter((entry) => entry.time_table_id === timetableId);
+  // Group data by day and time
   const groupedData = filteredData.reduce((acc, entry) => {
     const day = entry.time_table_block_day;
     const time = entry.time_table_block_time;
@@ -43,57 +35,54 @@ function TimeTable({ data, timetableId }) {
       acc[day] = {};
     }
 
+    // Find the closest matching time slot
     const closestTimeSlot = findClosestTimeSlot(time);
 
     if (!acc[day][closestTimeSlot]) {
       acc[day][closestTimeSlot] = [];
     }
-
     acc[day][closestTimeSlot].push(entry);
     return acc;
   }, {});
 
+  // Define days
   const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
   return (
-    <div>
-      <button className="generate-pdf-button" onClick={generatePDF}>Generate PDF</button>
-      <div className="timetable" ref={timetableRef}>
-        <table>
-          <thead>
-            <tr>
-              <th>Time</th>
+    <div className="timetable">
+      <table>
+        <thead>
+          <tr>
+            <th>Time</th>
+            {days.map((day) => (
+              <th key={day}>{day}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {timeSlots.map((timeSlot, index) => (
+            <tr key={index}>
+              <td>{timeSlot}</td>
               {days.map((day) => (
-                <th key={day}>{day}</th>
+                <td key={day}>
+                  {groupedData[day] && groupedData[day][timeSlot] ? (
+                    groupedData[day][timeSlot].map((entry) => (
+                      <div className="timetable-entry" key={entry.time_table_block_id}>
+                        <p>{entry.time_table_block_subject}</p>
+                        <p>{entry.time_table_block_faculty}</p>
+                        <p>{entry.time_table_block_room_no}</p>
+                      </div>
+                    ))
+                  ) : (
+                    ''
+                  )}
+                </td>
               ))}
             </tr>
-          </thead>
-          <tbody>
-            {timeSlots.map((timeSlot, index) => (
-              <tr key={index}>
-                <td>{timeSlot}</td>
-                {days.map((day) => (
-                  <td key={day}>
-                    {groupedData[day] && groupedData[day][timeSlot] ? (
-                      groupedData[day][timeSlot].map((entry) => (
-                        <div className="timetable-entry" key={entry.time_table_block_id}>
-                          <p>{entry.time_table_block_subject}</p>
-                          <p>{entry.time_table_block_faculty}</p>
-                          <p>{entry.time_table_block_room_no}</p>
-                        </div>
-                      ))
-                    ) : (
-                      ''
-                    )}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
-
 export default TimeTable;
