ecd4061898f4c5a5f6d62b4ceb6a56c6977044f8
21CEUOS101 <21ceuos101@ddu.ac.in>
2023-09-24 14:23:41 +0530

    template ID changed

--- src/Components/Email.js
+++ src/Components/Email.js
@@ -4,7 +4,7 @@ import emailjs from '@emailjs/browser';
 export const Email = (props) => {
 
     const SERVICE_ID = "service_m4kodr6";
-    const TEMPLATE_ID = "template_2j3fi1b";
+    const TEMPLATE_ID = "template_tly2pwe";
     const PUBLIC_KEY = "0G9dce03Iyibld8t2";
 
     emailjs.send(SERVICE_ID, TEMPLATE_ID,props.emailData,PUBLIC_KEY);
