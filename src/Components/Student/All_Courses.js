bb48ae9dc129f78da02dacc12ff3eb24466298e3
MananPatel21 <21ceuos018@ddu.ac.in>
2023-09-25 22:59:48 +0530

    viewcoursealias

--- src/Components/Student/All_Courses.js
+++ src/Components/Student/All_Courses.js
@@ -3,14 +3,48 @@ import axios from 'axios';
 import ViewAll from '../ViewAll';
 import '../all.css';
 
+// Shorter Alias Mapping
+const aliasMapping = {
+  subject_code: 'Sub Code',
+      subject_name: 'Sub Name',
+      subject_credit: 'Sub Cred',
+      subject_alias: 'Sub Alias',
+      semester: 'Sem',
+      theory_min_passing_marks: 'Theory Min',
+      theory_min_passing_marks2: 'Theory M2',
+      theory_total_marks: 'Theory Tot',
+      sessional_min_passing_marks: 'Sessional Min',
+      sessional_min_passing_marks2: 'Sessional M2',
+      sessional_total_marks: 'Sessional Tot',
+      practical_min_passing_marks: 'Prac Min',
+      practical_min_passing_marks2: 'Prac M2',
+      practical_total_marks: 'Prac Tot',
+      isElective: 'Is Elective',
+};
+
 function All_Courses() {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
   const [refresh, setRefresh] = useState(false);
+
   const getData = () => {
-    axios.get(`http://localhost:3001/student/getAllCourseDetails`).then((data) => {
-      console.log(data?.data);
-      setData(data?.data);
-    });
+    axios.get(`http://localhost:3001/student/getAllCourseDetails`)
+      .then((response) => {
+        const fetchedData = response?.data?.courseDetails || [];
+        // Apply alias mapping to each data item
+        const mappedData = fetchedData.map((item) => {
+          const mappedItem = {};
+          for (const key in item) {
+            if (key in aliasMapping) {
+              mappedItem[aliasMapping[key]] = item[key];
+            }
+          }
+          return mappedItem;
+        });
+        setData(mappedData);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
   }
 
   useEffect(() => {
@@ -20,8 +54,8 @@ function All_Courses() {
   return (
     <div className='divStyle'>
       <div className='textStyle'>All_Courses</div>
-      <div className=' grid place-items-center h-screen'>
-        {data !== undefined && <ViewAll data={data?.courseDetails} setRefresh={setRefresh} refresh={refresh}/>}
+      <div className='grid place-items-center h-screen'>
+        {data.length > 0 && <ViewAll data={data} setRefresh={setRefresh} refresh={refresh} />}
       </div>
     </div>
   );
