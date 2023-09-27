1f148c2b83adf10efae7a6541f7622e9c6d9eccd
MananPatel21 <21ceuos018@ddu.ac.in>
2023-09-27 11:14:01 +0530

    profile

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import './profileStyles.css';
 
 function Profile(props) {
   const user = props.data;
 
   return (
-    <div className="profile-container w-auto px-3 py-3 h-fit">
-      {Object?.keys(user)?.map((key,index) => {
-        if (index !== 0)
-        {
-          return (
-            <div className="profile-info" key={index}>
-              <span className="profile-key">{key}:</span>
-              <span className="profile-value">{JSON.stringify(user[key])}</span>
-            </div>
-          );
-        }
-      })}
+    <div className="bg-white shadow-md rounded my-6">
+      <div className="p-4">
+        <table className="min-w-max w-full table-auto">
+          <tbody className="text-gray-600 text-sm font-light">
+            {Object.keys(user).map((key, index) => {
+              if (index !== 0) {
+                return (
+                  <tr key={index} className="border-b border-gray-200 hover:bg-gray-100">
+                    <td className="px-6 py-4 whitespace-nowrap font-bold">{key}:</td>
+                    <td className="px-6 py-4 whitespace-nowrap">{user[key]}</td>
+                  </tr>
+                );
+              }
+              return null;
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
