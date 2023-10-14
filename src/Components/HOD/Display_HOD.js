64b0f07f34fb3a9c309e242d740f822964326f03
manthan161203 <manthanlp3204@gmail.com>
2023-10-14 11:56:00 +0530

    display for HOD change

--- src/Components/HOD/Display_HOD.js
+++ src/Components/HOD/Display_HOD.js
@@ -1,33 +1,31 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Profile from '../Profile';
 
 function Display_HOD() {
-
-  const {id} = useParams();
+  const { id } = useParams();
   const [data, setData] = useState();
 
   const getData = () => {
-    axios.get(`http://localhost:3001/hod/getHODDetails/${id}`).then((data) => {
-      console.log(data?.data);
-      setData(data?.data);
-    })
+    axios.get(`http://localhost:3001/hod/getHODDetails/${id}`).then((response) => {
+      console.log(response?.data);
+      setData(response?.data);
+    });
   }
 
   useEffect(() => {
     getData();
-  },[])
+  }, []);
+
   return (
-    <>
-          <div>Display_HOD</div>
-          <div>
-              {
-                  data !== undefined && <Profile data={data}/>
-              }
-          </div>
-    </>
-  )
+    <div className='w-full text-center bg-gray-200 p-4'>
+      <div className='text-2xl font-bold mb-4'>Display_HOD</div>
+      <div className='grid grid-row-1'>
+        {data !== undefined && <Profile data={data} />}
+      </div>
+    </div>
+  );
 }
 
-export default Display_HOD
+export default Display_HOD;
