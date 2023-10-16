d2c831a30d234026b31e9bc2926362a46c7ff6b2
manthan161203 <manthanlp3204@gmail.com>
2023-10-16 21:14:23 +0530

    Hello

--- src/Components/Student/Display_Student.js
+++ src/Components/Student/Display_Student.js
@@ -1,18 +1,17 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Profile from '../Profile';
 
 function Display_Student() {
-
-  const {id} = useParams();
+  const { id } = useParams();
   const [data, setData] = useState();
 
   const getData = () => {
-    axios.get(`http://localhost:3001/student/getStudentByRollNumber/${id}`).then((data) => {
-      console.log(data?.data);
-      setData(data?.data);
-    })
+    axios.get(`http://localhost:3001/student/getStudentByRollNumber/${id}`).then((response) => {
+      console.log(response?.data);
+      setData(response?.data);
+    });
   }
 
   useEffect(() => {
@@ -22,23 +21,32 @@ function Display_Student() {
   const studentDetails = data?.studentDetails[0];
   const studentGuardianInfo = data?.studentGuardianInfo[0];
   const studentOtherDetails = data?.studentOtherDetails[0];
+
   return (
     <div className='w-full text-center bg-gray-200 p-4'>
-          <div className='text-2xl font-bold mb-4'>Display_Student</div>
-          <div className='grid grid-row-3'>
-              {
-                  data !== undefined && <Profile data={studentDetails !== undefined && studentDetails}/>
-              }
-              {
-                  data !== undefined && <Profile data={studentGuardianInfo !== undefined && studentGuardianInfo}/>
-              }
-              {
-                  data !== undefined && <Profile data={studentOtherDetails !== undefined && studentOtherDetails}/>
-              }
-              
-          </div>
+      <div className='text-2xl font-bold mb-4'>Display_Student</div>
+      <div className='grid grid-row-3'>
+        {data !== undefined && (
+          <>
+            <h1>Student Details</h1>
+            <Profile data={studentDetails !== undefined && studentDetails} />
+          </>
+        )}
+        {data !== undefined && (
+          <>
+            <h1>Guardian Information</h1>
+            <Profile data={studentGuardianInfo !== undefined && studentGuardianInfo} />
+          </>
+        )}
+        {data !== undefined && (
+          <>
+            <h1>Other Details</h1>
+            <Profile data={studentOtherDetails !== undefined && studentOtherDetails} />
+          </>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Display_Student
+export default Display_Student;
